0eee4f0c8382f32851fdef6d4a7a161ca58e04de
Pablo Zaidenvoren <pabloz@lagash.com>
2017-04-24 16:32:55 -0300

    removed test constructor

--- java/src/main/java/dockerapi/Container.java
+++ java/src/main/java/dockerapi/Container.java
@@ -9,15 +9,6 @@ public class Container {
     public String image;
     public boolean isRunning;
 
-    public Container() {
-        this.state = "s1";
-        this.status = "stat1";
-        this.name = "n1";
-        this.id = "2183h182qn8d29";
-        this.image = "account/image";
-        this.isRunning = true;
-    }
-
     public Container(com.github.dockerjava.api.model.Container c, InspectContainerResponse.ContainerState state) {
         this.state = state.getStatus();
         this.status = c.getStatus();
